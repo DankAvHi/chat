c04f75f4b87fd0c7a848e864b26b1dfc666f0532
DankAvHi <thexcat86@gmail.com>
2023-04-11 01:45:51 +0300

    [crt] startApp function

--- chat-server/src/app/index.ts
+++ chat-server/src/app/index.ts
@@ -1,2 +1,6 @@
 import express from "express";
+import { startupLogger } from "./helpers/startupLogger";
+
 const app = express();
+
+export const startApp = () => app.listen(8000, startupLogger(8000));
