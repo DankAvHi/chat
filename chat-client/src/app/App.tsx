abb48fd79b6f11dfad93b9dd15d7a60054aba469
DankAvHi <thexcat86@gmail.com>
2023-04-11 04:08:13 +0300

    [add] global styles to app, some proto of layout

--- chat-client/src/app/App.tsx
+++ chat-client/src/app/App.tsx
@@ -1,5 +1,31 @@
+import { GlobalStyles } from "./styles";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <>
+            <GlobalStyles />
+            <div className="App">
+                <ul>
+                    <li></li>
+                </ul>
+                <main>
+                    <header>
+                        <h2>Name</h2>
+                        <div>
+                            <button>...</button>
+                        </div>
+                    </header>
+
+                    <div></div>
+
+                    <form>
+                        <input type="text" />
+                        <button>send</button>
+                    </form>
+                </main>
+            </div>
+        </>
+    );
 }
 
 export default App;
