ee96e92492358011fa4c73fd98b07c0a33caa3eb
DankAvHi <thexcat86@gmail.com>
2023-04-11 04:06:15 +0300

    [add] acces to env from vite config, change port

--- chat-client/vite.config.ts
+++ chat-client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return defineConfig({
+        plugins: [react()],
+
+        server: { port: Number(process.env.VITE_PORT) || 3000 },
+    });
+};
