309ba5e0fcb5b320b902eda46e4efa10cebe457f
DankAvHi <thexcat86@gmail.com>
2023-04-16 00:43:04 +0300

    [crt] mock files for user entity

---
+++ chat-client/src/entities/user/mock/index.tsx
@@ -0,0 +1,7 @@
+import userContactAvatarMock from "./avatar-contact.jpg";
+import userAvatarMock from "./avatar.jpg";
+
+export const userName = "DankAvhi";
+export const userContactName = "Shizophrenia";
+
+export { userAvatarMock, userContactAvatarMock };
