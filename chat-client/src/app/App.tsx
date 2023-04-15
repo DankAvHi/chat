ef8be9b3f346f93ac112ced50ede5d325eed9938
DankAvHi <thexcat86@gmail.com>
2023-04-16 00:46:06 +0300

    [add] Nav widget; providers and client for tRPC and react query

--- chat-client/src/app/App.tsx
+++ chat-client/src/app/App.tsx
@@ -1,15 +1,32 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Navigation } from "@/widgets";
+import { trpc } from "@/shared/api";
 import { GlobalStyles } from "./styles";
 
 function App() {
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: `http://${import.meta.env.VITE_SERVER_HOST}:${import.meta.env.VITE_SERVER_PORT}/api`,
+                }),
+            ],
+        }),
+    );
     return (
-        <>
-            <GlobalStyles />
-            <div className="App">
-                <nav></nav>
-                <Outlet />
-            </div>
-        </>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                <GlobalStyles />
+                <div className="App">
+                    <Navigation />
+                    <Outlet />
+                </div>
+            </QueryClientProvider>
+        </trpc.Provider>
     );
 }
 
