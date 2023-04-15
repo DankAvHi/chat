25e10eedd37b945b0350bc844486d52a0e203a00
DankAvHi <thexcat86@gmail.com>
2023-04-16 02:33:59 +0300

    [add] signJWT func to auth module

---
+++ server/src/auth/index.tsx
@@ -0,0 +1,14 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { config } from "../config";
+
+export const signJwt = (
+    payload: Object,
+    key: "JWT_ACCES_PRIVATE_KEY" | "JWT_REFRESH_PRIVATE_KEY",
+    options: SignOptions = {},
+) => {
+    const privateKey = Buffer.from(config[key], "base64").toString("ascii");
+    return jwt.sign(payload, privateKey, {
+        ...(options && options),
+        algorithm: "RS256",
+    });
+};
