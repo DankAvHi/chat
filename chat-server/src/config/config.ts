5acc9dc0b55b2c08383193f1c2d51da1d3517b0a
DankAvHi <thexcat86@gmail.com>
2023-04-15 15:23:46 +0300

    [chg] fully rewrite config module alghoritm to filter process env and checking for required fields

--- chat-server/src/config/config.ts
+++ chat-server/src/config/config.ts
@@ -1,16 +1,48 @@
-import { Config } from "./config.d";
+import { filterObject } from "../helpers/filterObject";
+import { errorLogger } from "../logger";
+import { Config, EnvConfig } from "./config.d";
 
-const generateConfig = (): (() => Config) => {
-    let config: Config | null = null;
-    return () => {
-        if (config) return config;
+const requiredFields = ["DATABASE_URL"];
+const notRequiredFields = ["PORT", "HOST"];
 
-        config = { PORT: process.env.PORT || "8000" };
+const envFields = Object.keys(process.env).filter(
+    (field) => requiredFields.includes(field) || notRequiredFields.includes(field),
+);
 
-        return config as Config;
-    };
+const checkAllRequiredFieldsExisted = () => {
+    const notExistedRequiredFields: string[] = [];
+
+    let isAllRequiredFieldsExisted = true;
+
+    for (let i = 0; i < requiredFields.length; i++) {
+        if (!envFields.includes(requiredFields[i])) {
+            notExistedRequiredFields.push(requiredFields[i]);
+            isAllRequiredFieldsExisted = false;
+        }
+    }
+
+    return { isAllRequiredFieldsExisted, notExistedRequiredFields };
 };
 
-const getConfig = generateConfig();
+const generateConfig = () => {
+    const { isAllRequiredFieldsExisted, notExistedRequiredFields } = checkAllRequiredFieldsExisted();
 
-export const config: Config = { PORT: getConfig().PORT };
+    if (!isAllRequiredFieldsExisted) {
+        const errorString = `Mising required fields in .env file:[${notExistedRequiredFields.map((field, index) =>
+            index === 0 ? `${field}` : ` ${field}`,
+        )}]`;
+        errorLogger(errorString);
+        throw new Error(errorString);
+    }
+
+    const envConfig: EnvConfig = filterObject(process.env, ([key, value]) => envFields.includes(key)) as EnvConfig;
+    const config: Config = {
+        ...envConfig,
+        isDevelopment: process.env.NODE_ENV == "development",
+        PORT: envConfig.PORT || 8000,
+        HOST: envConfig.HOST || "localhost",
+    };
+    return config;
+};
+
+export const config = generateConfig();
