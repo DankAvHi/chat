7fd7dcd32ac17f6d0d95d69589a28109a83cc0cd
DankAvHi <thexcat86@gmail.com>
2023-04-16 00:39:07 +0300

    [add] configs, appRouter for app instance; add check connection to startup

--- chat-server/src/app/index.ts
+++ chat-server/src/app/index.ts
@@ -1,6 +1,16 @@
 import express from "express";
-import { startupLogger } from "./helpers/startupLogger";
+import { startupLogger } from "./helpers";
+import { config, corsConfig } from "../config";
+import { checkDatabaseConnection } from "../prisma";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter, createContext } from "../tRPC";
+import cors from "cors";
 
 const app = express();
 
-export const startApp = () => app.listen(8000, startupLogger(8000));
+app.use(cors(corsConfig));
+
+app.use("/api", trpcExpress.createExpressMiddleware({ router: appRouter, createContext }));
+
+export const startApp = () =>
+    checkDatabaseConnection().then(() => app.listen(config.PORT, startupLogger(config.HOST, config.PORT)));
