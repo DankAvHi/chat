be31881542580c7c8dcc2d28de428ff10b0a12ad
DankAvHi <thexcat86@gmail.com>
2023-04-15 15:24:12 +0300

    [chg] little changes in startup logger

--- chat-server/src/app/helpers/startupLogger.ts
+++ chat-server/src/app/helpers/startupLogger.ts
@@ -1,5 +1,10 @@
+import { config } from "../../config/config";
 import { logger } from "../../logger";
 import clc from "cli-color";
 
-export const startupLogger = (port: number) => () =>
-    logger(`${clc.greenBright(`[🚀 StartUp]`)} App started succefuly on port:${port}`);
+export const startupLogger = (host: string, port: number) => () =>
+    logger(
+        `${clc.greenBright(`[🚀 StartUp]`)} App started succefuly in ${
+            config.isDevelopment ? "development" : "production"
+        } mode, on http://${host}:${port}`,
+    );
