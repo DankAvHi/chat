6971f3ff4bfc228c604c1e1e20b2fb52e2157d9d
DankAvHi <thexcat86@gmail.com>
2023-04-16 02:49:54 +0300

    [crt] verifyJWT func in auth module

--- server/src/auth/index.tsx
+++ server/src/auth/index.tsx
@@ -1,5 +1,6 @@
 import jwt, { SignOptions } from "jsonwebtoken";
 import { config } from "../config";
+import { errorLogger } from "../logger";
 
 export const signJwt = (
     // eslint-disable-next-line
@@ -12,4 +13,14 @@ export const signJwt = (
         ...(options && options),
         algorithm: "RS256",
     });
+};
+
+export const verifyJwt = <T,>(token: string, key: "JWT_ACCES_PUBLIC_KEY" | "JWT_REFRESH_PUBLIC_KEY"): T | null => {
+    try {
+        const publicKey = Buffer.from(config[key], "base64").toString("ascii");
+        return jwt.verify(token, publicKey) as T;
+    } catch (error) {
+        errorLogger(error as string);
+        return null;
+    }
 };
