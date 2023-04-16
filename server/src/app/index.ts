c6d14c31c9f7795cb35183f809b438f3dda7c578
DankAvHi <thexcat86@gmail.com>
2023-04-17 00:56:06 +0300

    [chg] prisma module import path; add check redis connection

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { startupLogger } from "./helpers";
 import { config, corsConfig } from "../config";
-import { checkDatabaseConnection } from "../prisma";
+import { checkPrismaDatabaseConnection } from "../db/prisma";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter, createContext } from "../tRPC";
 import cors from "cors";
+import { connectRedis } from "../db";
 
 const app = express();
 
@@ -13,4 +14,6 @@ app.use(cors(corsConfig));
 app.use("/api", trpcExpress.createExpressMiddleware({ router: appRouter, createContext }));
 
 export const startApp = () =>
-    checkDatabaseConnection().then(() => app.listen(config.PORT, startupLogger(config.HOST, config.PORT)));
+    checkPrismaDatabaseConnection()
+        .then(async () => await connectRedis())
+        .then(() => app.listen(config.PORT, startupLogger(config.HOST, config.PORT)));
