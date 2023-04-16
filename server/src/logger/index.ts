fddb153739ebcabf949b7031e0da983eeaad147d
DankAvHi <thexcat86@gmail.com>
2023-04-17 00:53:25 +0300

    [fix] add export of succesLogger to index logers

--- server/src/logger/index.ts
+++ server/src/logger/index.ts
@@ -2,3 +2,4 @@ export { logger } from "./logger";
 export { infoLogger } from "./infoLogger";
 export { warnLogger } from "./warnLogger";
 export { errorLogger } from "./errorLogger";
+export { succesLogger } from "./succesLogger";
