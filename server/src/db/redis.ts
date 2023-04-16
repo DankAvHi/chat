9e2daff8fc6a6aa2135c8266bb3dcd19742bc81c
DankAvHi <thexcat86@gmail.com>
2023-04-17 00:54:07 +0300

    [crt] redis module

---
+++ server/src/db/redis.ts
@@ -0,0 +1,21 @@
+import { createClient } from "redis";
+import { config } from "../config";
+import { errorLogger } from "../logger";
+import { succesLogger } from "../logger";
+
+const redisClient = createClient({
+    url: `redis://${config.REDIS_HOST}:${config.REDIS_PORT}`,
+    password: config.REDIS_PASSWORD,
+});
+
+export const connectRedis = async () => {
+    await redisClient.connect().catch((err) => {
+        errorLogger(err);
+        throw err;
+    });
+    succesLogger(`Connected to Redis cache db`);
+
+    redisClient.on("error", (error) => errorLogger(error as string));
+};
+
+export { redisClient };
