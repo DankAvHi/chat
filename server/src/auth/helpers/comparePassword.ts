a0a44db2d041f28b21ab2ae6519b635f39d65a1f
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:21:21 +0300

    [crt] comparePassword helper

---
+++ server/src/auth/helpers/comparePassword.ts
@@ -0,0 +1,6 @@
+import * as bcrypt from "bcryptjs";
+
+export const comparePassword = async (
+    password: string,
+    hashedPassword: string,
+) => await bcrypt.compare(password, hashedPassword);
