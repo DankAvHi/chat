cdfaadf7008e8e6d0534b930388200fd08f36aba
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:16:32 +0300

    [add] api zod schemas and type to project shared

--- shared/index.ts
+++ shared/index.ts
@@ -1,1 +1,6 @@
 export type { AppRouter } from "../server/src/tRPC/router";
+
+export type { RegisterUserInput } from "../server/src/auth/validation";
+export { registerSchema } from "../server/src/auth/validation";
+export type { LoginUserInput } from "../server/src/auth/validation";
+export { loginSchema } from "../server/src/auth/validation";
