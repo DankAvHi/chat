8d80de8b56bf7405d4619030fd174ae4ed267421
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:20:22 +0300

    [crt] register controller

---
+++ server/src/auth/controllers/register.controller.ts
@@ -0,0 +1,21 @@
+import { TRPCError } from "@trpc/server";
+import { publicProcedure } from "../../tRPC";
+import { isUserExisted, registerUser } from "../auth.service";
+import { registerSchema } from "../validation";
+
+export const register = publicProcedure
+    .input(registerSchema)
+    .mutation(async ({ input }) => {
+        const { login, password } = input;
+
+        if (await isUserExisted(login)) {
+            throw new TRPCError({
+                code: "CONFLICT",
+                message: `User with login: "${login}" is already existed`,
+            });
+        }
+
+        const user = registerUser(login, password);
+
+        return { status: "succes" };
+    });
