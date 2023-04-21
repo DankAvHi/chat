8c6b84b251cddfdf446641b553f77436b8863f08
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:09:57 +0300

    [add] fluent ui theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./app/Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <Router />
+        <FluentProvider theme={webLightTheme}>
+            <Router />
+        </FluentProvider>
     </React.StrictMode>,
 );
