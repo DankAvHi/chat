263a26316275d882bb901af258faba252178ab4e
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:15:37 +0300

    [ref] move trpc config to sep file

--- client/src/shared/api/tRPC.tsx
+++ client/src/shared/api/tRPC.tsx
@@ -1,4 +1,21 @@
+import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "@project/shared";
 
 export const trpc = createTRPCReact<AppRouter>();
+export const createTRPCClient = () =>
+    trpc.createClient({
+        links: [
+            httpBatchLink({
+                url: `http://${import.meta.env.VITE_SERVER_HOST}:${
+                    import.meta.env.VITE_SERVER_PORT
+                }/api`,
+                fetch(url, options) {
+                    return fetch(url, {
+                        ...options,
+                        credentials: "include",
+                    });
+                },
+            }),
+        ],
+    });
