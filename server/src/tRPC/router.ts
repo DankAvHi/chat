3f52e51be8a805ade70d46239119c57f30eacacc
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:25:22 +0300

    [add] auth router to appRouter

--- server/src/tRPC/router.ts
+++ server/src/tRPC/router.ts
@@ -1,8 +1,10 @@
+import { authRouter } from "../auth";
 import { userRouter } from "../user";
 import { router } from "./tRPC";
 
 export const appRouter = router({
     user: userRouter,
+    auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
