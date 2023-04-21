76db50a8c0b77950e1c4c2fdfc8ab47f384319a1
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:14:48 +0300

    [crt] styles for auth page and forms

---
+++ client/src/pages/Auth/Auth.styles.tsx
@@ -0,0 +1,32 @@
+import { makeStyles } from "@fluentui/react-components";
+import styled from "styled-components";
+
+export const AuthStyles = {
+    Auth: styled.main`
+        width: 100%;
+        min-height: calc(100svh - 81px);
+
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+    `,
+
+    Form: styled.form`
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    `,
+};
+
+export const useAuthFluentStyles = makeStyles({
+    Card: {
+        paddingRight: "12px",
+        paddingLeft: "12px",
+        paddingTop: "12px",
+        paddingBottom: "12px",
+    },
+    Heading: {
+        paddingBottom: "18px",
+        paddingTop: "18px",
+    },
+});
