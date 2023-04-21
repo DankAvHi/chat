fd6f1a5f32bc042f6d2dfd9ca32add0924af810e
DankAvHi <thexcat86@gmail.com>
2023-04-21 16:26:05 +0300

    [chg] rename trpc obj, add export trpc middleware

--- server/src/tRPC/tRPC.ts
+++ server/src/tRPC/tRPC.ts
@@ -1,7 +1,8 @@
 import { initTRPC } from "@trpc/server";
 import { Context } from "./context";
 
-const t = initTRPC.context<Context>().create();
+const trpc = initTRPC.context<Context>().create();
 
-export const router = t.router;
-export const publicProcedure = t.procedure;
+export const router = trpc.router;
+export const publicProcedure = trpc.procedure;
+export const middleware = trpc.middleware;
