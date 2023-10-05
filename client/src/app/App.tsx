971a314194631f4f52bf98093ce611f665563463
DankAvHi <thexcat86@gmail.com>
2023-10-05 17:05:44 +0300

    [add] mock messages to html layout

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -4,7 +4,65 @@ function App() {
     return (
         <div className={styles.App}>
             <div className={styles.chat}>
-                <div className={styles.messages}></div>
+                <div className={styles.messages}>
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
+                        <p className={styles.message}>{"Message with length of ecuator"}</p>
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+
+                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
+                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
+                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
+                    </div>
+                </div>
                 <form className={styles.chatForm}>
                     <input type="text" placeholder={"Enter your message here..."} className={styles.chatInput} />
                     <input type="submit" value="Send" className={styles.chatSubmit} />
