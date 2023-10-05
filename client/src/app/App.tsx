8d52dc28122299f6f257a32fff957951c1b69bfb
DankAvHi <thexcat86@gmail.com>
2023-10-05 16:33:32 +0300

    [add] html layout for chat

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,17 @@
 import styles from "./App.module.css";
 
 function App() {
-    return <div className={styles.App}></div>;
+    return (
+        <div className={styles.App}>
+            <div className={styles.chat}>
+                <div className={styles.messages}></div>
+                <form className={styles.chatForm}>
+                    <input type="text" className={styles.chatInput} />
+                    <input type="submit" value="Send" className={styles.chatSubmit} />
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
