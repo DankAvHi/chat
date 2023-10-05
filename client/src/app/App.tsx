8463c57ab892018ae9738d638f0b9105993a7cdf
DankAvHi <thexcat86@gmail.com>
2023-10-05 16:44:09 +0300

    [add] placeholder to input

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,7 +6,7 @@ function App() {
             <div className={styles.chat}>
                 <div className={styles.messages}></div>
                 <form className={styles.chatForm}>
-                    <input type="text" className={styles.chatInput} />
+                    <input type="text" placeholder={"Enter your message here..."} className={styles.chatInput} />
                     <input type="submit" value="Send" className={styles.chatSubmit} />
                 </form>
             </div>
