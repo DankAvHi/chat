9ae7e44af7f952965703aad0a7660636650744cc
DankAvHi <thexcat86@gmail.com>
2023-10-12 23:26:50 +0300

    some changes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,18 @@
 import styles from "./App.module.css";
+import io from "socket.io-client";
 
 function App() {
+    const socket = io("http://localhost:80");
+    socket.on("connect", () => {
+        console.log(socket.id);
+    });
+
+    const click = (event: React.MouseEventHandler<HTMLInputElement>) => {
+        socket.send("hello");
+    };
+
+    const send = () => {};
+
     return (
         <div className={styles.App}>
             <div className={styles.chat}>
@@ -14,58 +26,10 @@ function App() {
                         <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
                         <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
                     </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
-                        <p className={styles.message}>{"Message with length of ecuator"}</p>
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
-                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
-                        <p className={styles.message}>{"Message with length of ecuator"}</p>
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
-                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
-                        <p className={styles.message}>{"Message with length of ecuator"}</p>
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
-                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
-                        <p className={styles.message}>{"Message with length of ecuator"}</p>
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
-                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_user}`}>
-                        <p className={styles.message}>{"Message with length of ecuator"}</p>
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
-
-                    <div className={`${styles.messageWrapper} ${styles.messageWrapper_friend}`}>
-                        <p className={styles.message}>{"Answer for Message with length of ecuator long text btw"}</p>{" "}
-                        <p className={styles.messageTime}>{"12:34 05.10.2023"}</p>
-                    </div>
                 </div>
-                <form className={styles.chatForm}>
+                <form onSubmit={send} className={styles.chatForm}>
                     <input type="text" placeholder={"Enter your message here..."} className={styles.chatInput} />
-                    <input type="submit" value="Send" className={styles.chatSubmit} />
+                    <input type="submit" onClick={click} value="Send" className={styles.chatSubmit} />
                 </form>
             </div>
         </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import Express from "express";
 import path from "path";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const CLIENT_PATH = path.resolve("client");
 const CLIENT_DIST_PATH = path.resolve(CLIENT_PATH, "dist");
@@ -11,6 +13,18 @@ app.use(Express.static(CLIENT_DIST_PATH));
 
 app.get("*", (_req, res) => res.sendFile(CLIENT_INDEX_PATH));
 
-app.listen(80, () => {
-    console.log("App started at http://localhost:80");
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+httpServer.listen(80, () => {
+    console.log("listening on http://localhost:80");
 });
